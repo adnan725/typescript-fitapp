eb1fe39155292042668be7ad751cf226de7cb041
Adnan Ali <adnanali6688@gmail.com>
2023-06-10 16:32:55 +0200

    corrected import

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
-import Logo from '@/assets/Logo.png';
+import Logo from "../../assets/Logo.png";
 import Link from './Link';
 
 type NavbarProps = {
@@ -17,8 +16,7 @@ const Navbar = ({selectedPage, setSelectedPage}: NavbarProps) => {
       <div className={`${flexBetween} fixed top-0 w-full z-30 py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
-            {/* <img src={Logo} alt="logo" /> */}
-            LOGO
+            <img src={Logo} alt="logo" />
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-small`}>
                 <Link 
